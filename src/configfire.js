2e5dc354a0193228815ec61efa5d09f34dddfc95
Fernando Santos <Fernando Santos>
2018-02-07 20:55:33 -0300

    principal

---
+++ src/configfire.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyALkZ_5YTZMMF4rdHnEEiDYHuO_l7C0Up8',
+  authDomain: 'vspa-15b54.firebaseapp.com',
+  databaseURL: 'https://vspa-15b54.firebaseio.com',
+  projectId: 'vspa-15b54',
+  storageBucket: 'vspa-15b54.appspot.com',
+  messagingSenderId: '853018899462'
+}
